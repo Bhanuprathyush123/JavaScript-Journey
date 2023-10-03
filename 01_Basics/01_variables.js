abe6bbf25add8045cca7f319bc4c84deb655e6c1
VarreyShivaGaneshBhanuPrathyush <prathyush.varrey123@gmail.com>
2023-10-03 13:22:09 +0530

    commiting the 01_varibales file and the modifications that are done

---
+++ 01_Basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 191101
+// => const a DT where You once you Initialize the value you can't 
+// Reinitialize and You cna't use the Variable Name again.
+
+let accountemail = "prap12r@gmai.com"
+// => let is a DT which can be Reintialized but you can't reinitialze the variable
+var accountPassword = "1234"
+// => var it is different from the other two since we can Reinitialze both variable and value of variable
+city = "Jaipur"
+
+console.log("CitlyName:",city," Email:",accountemail," Id:",accountId)
+  // => console.log() -> is a Function that prints the Parameters that are given 
+  // in the open "(" and closing ")" brackets.as You seen above
+accountemail = "send@mail.com"
