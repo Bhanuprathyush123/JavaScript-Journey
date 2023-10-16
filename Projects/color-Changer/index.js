f1ff009d0b2c392a090919b8b0bd63c49f2b5dc3
VarreyShivaGaneshBhanuPrathyush <prathyush.varrey123@gmail.com>
2023-10-16 12:13:24 +0530

    Added .\Projects\color-Changer\

---
+++ Projects/color-Changer/index.js
@@ -0,0 +1,30 @@
+let buttons = document.querySelectorAll('.buttons')
+// console.log(buttons);
+let body = document.querySelector('body')
+
+
+buttons.forEach((button) => {
+     // console.log(button);
+     button.addEventListener('click', function (event) {
+          console.log(event);
+
+          // console.log(event.target);
+          if (event.target.id === 'blue') {
+               body.style.backgroundColor = event.target.id
+          }
+          else if (event.target.id === 'gray') {
+               body.style.backgroundColor = event.target.id
+          }
+          else if (event.target.id === 'green') {
+               body.style.backgroundColor = event.target.id
+          }
+          else if (event.target.id === 'orange') {
+               body.style.backgroundColor = event.target.id
+          } 
+          else if (event.target.id === 'greenyellow') {
+               body.style.backgroundColor = event.target.id
+          } else {
+               body.style.backgroundColor = 'white'
+          }
+     })
+})
